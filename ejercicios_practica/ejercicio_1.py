1cb443f0af96464072d91446b15c4226525c4170
Damian Augusto <60717309+damianaugusto1501@users.noreply.github.com>
2022-07-24 20:04:48 -0300

    Update ejercicio_1.py

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -1,49 +1,28 @@
 # Tipos de variables [Python]
-# Ejemplos de clase
+# Ejercicios de práctica
 
 # Autor: Inove Coding School
 # Version: 2.0
 
-# Ejemplos varialbles numéricas (números enteros --> int)
-numero_1 = 2
-numero_2 = 4
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
 
-suma = numero_1 + numero_2   # Operamos la suma
-# Imprimimos en consola el resultado
-print('El resultado de la suma es', suma)
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+# Almacenar el valor de la suma en una variable
+# ej:
+# operacion = .....
 
-resta = numero_1 - numero_2  # Operamos la resta
+# Imprimir en pantalla el resultado de la suma
+# print(....)
 
-# Imprimimos en consola el resultado con dos métodos distintos
-print('El resultado de restar', numero_1, 'y', numero_2, 'es', resta)
+# Repita el procedimiento para realizar la resta
 
-# ¿Qué sucede si una variable no fue definida e intento imprimir su valor?
-# print('El valor de numero es',numero)
+numero_1 = 5
+numero_2 = 7
 
-# Ahora realizaremos ejemplos con números decimales --> float
-numero_3 = 3.0
-numero_4 = 4.5
+suma = numero_1 + numero_2
+print("la suma es igual a ",suma)
 
-suma = numero_3 + numero_4  # Operamos la suma
-# Imprimimos en consola el resultado
-print('El resultado de la suma es', suma)
-
-resta = numero_4 - numero_3  # Operamos la resta
-
-# Imprimimos en consola el resultado con dos métodos distintos
-print('El resultado de restar', numero_4, 'y', numero_3, 'es', resta)
-
-# Ahora realizaremos un ejemplo numérico mixto
-
-suma = numero_3 + numero_1   # Operamos la suma
-# Imprimimos en consola el resultado
-print('El resultado sumar', numero_3, 'y', numero_1, 'es', suma)
-
-resta = numero_3 - numero_1  # Operamos la resta
-
-# Imprimimos en consola el resultado con dos métodos distintos
-print('El resultado de restar', numero_3, 'y', numero_1, 'es', resta)
-
-# Ensayo de división de números enteros
-division = numero_1 / numero_2
-print('El resultado dividir', numero_1, 'y', numero_2, 'es', division)
+resta = numero_1 - numero_2
+print("la resta es igual a ",resta)
